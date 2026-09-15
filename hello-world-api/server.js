9123f6f4f75d814a76c9fd8c578f3ec9fcf8d31f
iRVINE <quynhngoc4076@gmail.com>
2026-09-16 00:15:35 +0700

    change file

--- hello-world-api/server.js
+++ hello-world-api/server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/api/hello", (req, res) => {
-    res.status(200).json({
-        message: "Hello World!"
-    });
+  res.status(200).json({
+    message: "Hello World!",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
